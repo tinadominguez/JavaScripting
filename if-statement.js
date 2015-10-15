e277494e42c61c2ad4a4d16ffc4cee3519d49ad9
Tina Dominguez <tina@tinadominguez.com>
2015-10-14 22:49:43 -0400

    equal signs

--- if-statement.js
+++ if-statement.js
@@ -1,6 +1,6 @@
-var fruit("orange");
+var fruit="orange";
 
-if (fruit > 5) {
+if (fruit.length > 5) {
   console.log("The fruit name has more than five characters.");
 } else {
   console.log("The fruit name has five characters or less.");
