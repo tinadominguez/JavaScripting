bfc720c8d6073758cabe384f24b45b937950a734
Tina Dominguez <tina@tinadominguez.com>
2015-10-14 23:12:54 -0400

    fixed for loop

--- for-loop.js
+++ for-loop.js
@@ -1,6 +1,6 @@
 var total = 0;
 var limit = 10;
-for (var i = 0; i < 10; i++) {
-  // log the numbers 0 through 9
-  console.log(total);
+for (i = 0; i < 10; i++) {
+  total += i;
 }
+console.log(total);
